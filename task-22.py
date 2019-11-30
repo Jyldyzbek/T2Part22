36641dee11b4c5891ae07efed3349e2d17da022e
jikish <jikish2103dk@gmail.com>
2019-11-30 15:49:43 +0600

    Part-2 Task-22

---
+++ task-22.py
@@ -0,0 +1,16 @@
+tF = int(input('Vedite °F: '))
+tC = int(input('Vedite °C: '))
+
+TC = 5/9 * (tF - 32)
+TF = 9/5 * tC + 32 
+
+print('Celsius', round(TC, 2))
+print('Farengate', round(TF, 2))
+
+
+
+
+
+
+
+#  a = (32 °F − 32) × 5/9 = 0 °C
